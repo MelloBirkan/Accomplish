57d845685d445b0d571bef755e7cce1ec8752d59
Mello Birkan <mgbirkan@icloud.com>
2024-03-02 20:34:26 -0300

    Initial Commit

---
+++ Accomplish/AccomplishApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccomplishApp.swift
+//  Accomplish
+//
+//  Created by Marcello Gonzatto Birkan on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AccomplishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
